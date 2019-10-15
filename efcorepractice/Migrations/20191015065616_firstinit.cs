1fc8137ec55a2d89166650695f88a0f557f0aa87
fahim <fantasizer1001@gmail.com>
2019-10-15 12:56:28 +0600

    sdsd

---
+++ efcorepractice/Migrations/20191015065616_firstinit.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace efcorepractice.Migrations
+{
+    public partial class firstinit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Company",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    DBServerDateTime = table.Column<DateTime>(nullable: false),
+                    DBServerUserID = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    CompanyPrefix = table.Column<string>(nullable: true),
+                    ParentCompanyID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Company", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Company");
+        }
+    }
+}
